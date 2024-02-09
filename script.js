d011b75e76654f6087503b8a2110af539834a489
vegetason <irakozepirlo@gmail.com>
2024-02-09 17:58:42 +0200

    Get the game halfway done in the console

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let newGame=document.querySelector('#newGame')
 let first=document.querySelector('#first');
 let second=document.querySelector('#second');
 let third=document.querySelector('#third');
@@ -7,7 +8,7 @@ let sixth=document.querySelector('#sixth');
 let seventh=document.querySelector('#seventh');
 let eighth=document.querySelector('#eighth');
 let nineth=document.querySelector('#nineth');
-let buttplays=document.querySelectorAll('button')
+let buttplays=document.querySelectorAll('.play')
 let i=1;
 let arr1,arr2,arr3;
 buttplays.forEach((button)=>{
@@ -19,24 +20,35 @@ buttplays.forEach((button)=>{
         arr1=[first.textContent,second.textContent,third.textContent];
         arr2=[fourth.textContent,fifth.textContent,sixth.textContent];
         arr3=[seventh.textContent,eighth.textContent,nineth.textContent];
-        return arr1,arr2,arr3;
+        button.setAttribute('disabled','disabled')
+        if(arr1[0]==='O' && arr1[1]==='O' && arr1[2]==='O') console.log('Player 1 won')
+        else if(arr1[0]==='X' && arr1[1]==='X' && arr1[2]==='X') console.log('Player 2 won')
+        else if(arr1[0]==='X' && arr2[0]==='X' && arr3[0]==='X') console.log('Player 2 won')
+        else if(arr1[0]==='O' && arr2[0]==='O' && arr3[0]==='O') console.log('Player 1 won')
+        else if(arr1[0]==='X' && arr2[0]==='X' && arr3[0]==='X') console.log('Player 2 won')
+        else if(arr1[0]==='X' && arr2[1]==='X' && arr3[2]==='X') console.log('Player 2 won')
+        else if(arr1[0]==='O' && arr2[1]==='O' && arr3[2]==='O') console.log('Player 1 won')
+        else if(arr1[2]==='X' && arr2[1]==='X' && arr3[0]==='X') console.log('Player 2 won')
+        else if(arr2[2]==='X' && arr2[1]==='X' && arr2[0]==='X') console.log('Player 2 won')
+        else if(arr2[2]==='O' && arr2[1]==='O' && arr2[0]==='O') console.log('Player 1 won')
+        else if(arr3[2]==='X' && arr3[1]==='X' && arr3[0]==='X') console.log('Player 2 won')
+        else if(arr3[2]==='O' && arr3[1]==='O' && arr3[0]==='O') console.log('Player 1 won')
+        else if(arr1[2]==='O' && arr2[2]==='O' && arr3[3]==='O') console.log('Player 1 won')
+        else if(arr1[2]==='X' && arr2[2]==='X' && arr3[3]==='X') console.log('Player 2 won')
+        else if(arr1[1]==='O'&& arr2[1]==='O' && arr3[1]==='O') console.log('Player 1 won')
+        else if(arr1[1]==='X'&& arr2[1]==='X' && arr3[1]==='X') console.log('Player 2 won')
+        else console.log('It\'s a draw')
+        console.log(arr1,arr2,arr3)
     })
-    return arr1,arr2,arr3;
 })
-if(arr1[0]==='O' && arr1[1]==='O' && arr1[2]==='O') console.log('Player 1 won')
-else if(arr1[0]==='X' && arr1[1]==='X' && arr1[2]==='X') console.log('Player 2 won')
-else if(arr1[0]==='X' && arr2[0]==='X' && arr3[0]==='X') console.log('Player 2 won')
-else if(arr1[0]==='O' && arr2[0]==='O' && arr3[0]==='O') console.log('Player 1 won')
-else if(arr1[0]==='X' && arr2[0]==='X' && arr3[0]==='X') console.log('Player 2 won')
-else if(arr1[0]==='X' && arr2[1]==='X' && arr3[2]==='X') console.log('Player 2 won')
-else if(arr1[0]==='O' && arr2[1]==='O' && arr3[2]==='O') console.log('Player 1 won')
-else if(arr1[2]==='X' && arr2[1]==='X' && arr3[0]==='X') console.log('Player 2 won')
-else if(arr2[2]==='X' && arr2[1]==='X' && arr2[0]==='X') console.log('Player 2 won')
-else if(arr2[2]==='O' && arr2[1]==='O' && arr2[0]==='O') console.log('Player 1 won')
-else if(arr3[2]==='X' && arr3[1]==='X' && arr3[0]==='X') console.log('Player 2 won')
-else if(arr3[2]==='O' && arr3[1]==='O' && arr3[0]==='O') console.log('Player 1 won')
-else if(arr1[2]==='O' && arr2[2]==='O' && arr3[3]==='O') console.log('Player 1 won')
-else if(arr1[2]==='X' && arr2[2]==='X' && arr3[3]==='X') console.log('Player 2 won')
-else if(arr1[1]==='O'&& arr2[1]==='O' && arr3[1]==='O') console.log('Player 1 won')
-else if(arr1[1]==='X'&& arr2[1]==='X' && arr3[1]==='X') console.log('Player 2 won')
-else console.log('It\'s a draw')
+        arr1=[first.textContent,second.textContent,third.textContent];
+        arr2=[fourth.textContent,fifth.textContent,sixth.textContent];
+        arr3=[seventh.textContent,eighth.textContent,nineth.textContent];
+
+newGame.addEventListener('click',()=>{
+    i=1;
+    buttplays.forEach(button=>{
+        button.removeAttribute('disabled')
+        button.textContent='';
+    })
+})
